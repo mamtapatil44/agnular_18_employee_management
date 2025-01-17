2711a7215bc92e02a276ef13dc92539b08d8b9aa
Mamta Patil <mamtapatil44@gmail.com>
2025-01-17 17:45:56 -0500

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { MasterComponent } from './components/master/master.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MasterComponent,RouterLink,RouterLinkActive],
+  imports: [RouterOutlet,MasterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
