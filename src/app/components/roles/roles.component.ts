e77bd132829e9be713112e7ea7e63f546e5e6108
Mamta Patil <mamtapatil44@gmail.com>
2025-01-16 19:11:51 -0500

    angular 18 project

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  
+
+}
