dfaaadb9903f094808764a3178fc429171d54906
Mamta Patil <mamtapatil44@gmail.com>
2025-01-16 20:10:35 -0500

    study of ngFor

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
+ roleList :IRole[] =[];
+ http = inject(HttpClient)
+
+  ngOnInit(): void {
+    console.log("in role component")
+    this.getAllRoles();
+  }
+
+  getAllRoles(){
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+      this.roleList = res.data;
+    })
+     
+  }
   
 
 }
